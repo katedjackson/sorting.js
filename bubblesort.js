a9869c9027aea974d6ad170ab7090353ab75dd6d
Ya-wen Alice <yawenalice@gmail.com>
2016-11-03 12:53:42 -0400

    update

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,26 @@
-function bubbleSort(array) {
-	
+function bubbleSort(arr) {
+
+  if (arr.length === 1) return arr;
+
+  function comparison(a, b) {
+    return a < b ? true : false;
+  }
+
+  function swap(arr, a, b) {
+    var hold = arr[b];
+    arr[b] = arr[a];
+    arr[a] = hold;
+  }
+
+
+	for(var x =0; x < arr.length; x = x+2) {
+    debugger;
+    if (!comparison(arr[x], arr[x+1])) {
+      swap(arr, x, x+1);
+    }
+  }
+
+
+
+  return arr;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -9,10 +9,10 @@ describe('Bubble Sort', function(){
     var arr = [3];
     expect( bubbleSort(arr) ).toEqual( [3] );
   });
-  
+
   it('handles multiple items', function(){
-  	var arr = ['b', 'd', 'a', 'c'];
-    expect( bubbleSort(arr) ).toEqual( ['a', 'b', 'c', 'd'] );
+  	var arr = ['e', 'd', 'c', 'a'];
+    expect( bubbleSort(arr) ).toEqual( ['a', 'c', 'd', 'e'] );
     var arr = [3, 2, 4, 1];
     expect( bubbleSort(arr) ).toEqual( [1, 2, 3, 4] );
   });
@@ -21,9 +21,9 @@ describe('Bubble Sort', function(){
 
 describe('A spy', function() {
 
-	
 
-	
+
+
 it("tracks the number of times swap was called", function() {
     expect(swap.calls.count()).toEqual(0);
 
